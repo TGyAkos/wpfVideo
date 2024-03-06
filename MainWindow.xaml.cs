09dea28de0ac0b510d5320fda81b5748b82fd091
TGyAkos <t.gyula.akos@gmail.com>
2024-03-06 12:39:56 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace WPFMultimedia
+{
+
+    public partial class MainWindow : Window
+    {
+        private bool mediaPlayerIsPlaying = false;
+        private bool userIsDraggingSlider = false;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += timer_Tick;
+            timer.Start();
+            mePlayer.Volume = 100;
+            mePlayer.Play();
+        }
+
+        void mediaPlay(Object sender, EventArgs e)
+        {
+            mePlayer.Play();
+        }
+
+        void mediaPause(Object sender, EventArgs e)
+        {
+            mePlayer.Pause();
+        }
+
+        void mediaMute(Object sender, EventArgs e)
+        {
+
+            if (mePlayer.Volume == 100)
+            {
+                mePlayer.Volume = 0;
+                muteButt.Content = "Listen";
+            }
+            else
+            {
+                mePlayer.Volume = 100;
+                muteButt.Content = "Mute";
+            }
+        }
+
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if ((mePlayer.Source != null) && (mePlayer.NaturalDuration.HasTimeSpan) && (!userIsDraggingSlider))
+            {
+                sliProgress.Minimum = 0;
+                sliProgress.Maximum = mePlayer.NaturalDuration.TimeSpan.TotalSeconds;
+                sliProgress.Value = mePlayer.Position.TotalSeconds;
+            }
+        }
+
+        private void sliProgress_DragStarted(object sender, DragStartedEventArgs e)
+        {
+            userIsDraggingSlider = true;
+        }
+
+        private void sliProgress_DragCompleted(object sender, DragCompletedEventArgs e)
+        {
+            userIsDraggingSlider = false;
+            mePlayer.Position = TimeSpan.FromSeconds(sliProgress.Value);
+        }
+
+        private void sliProgress_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            TimeSpan.FromSeconds(sliProgress.Value).ToString(@"hh\:mm\:ss");
+        }
+
+        private void Grid_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            mePlayer.Volume += (e.Delta > 0) ? 0.1 : -0.1;
+        }
+    }
+}
